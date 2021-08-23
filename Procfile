078bd2a5367d7c6af67ed4fb04de6e4c979eb68d
muhayminul <muhayminul96@gmail.com>
2021-08-23 13:56:42 +0600

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn brainstem.wsgi --log-file -
---
+++ brainstem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for brainstem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brainstem.settings')
+
+application = get_wsgi_application()
